c1638b3385946777c38d0f181a130233c51051b0
SSCWebs <sscwebservices@gmail.com>
2020-12-08 21:51:17 -0500

    removed server

--- server/middleware/session.middleware.ts
+++
@@ -1,8 +0,0 @@
-import session from 'express-session';
-
-const sessionConfiguration = {
-    secret: 'descretion',
-    cookie: { secure: false },
-};
-
-export const sessionMiddleware = session(sessionConfiguration);
--- server/router/auth.router.ts
+++
@@ -1,7 +0,0 @@
-import express from 'express';
-
-export const authRouter = express.Router();
-
-authRouter.get('/check-session', (req,res) => {
-    res.json(req.session)
-})
